c06dea5fb4c5713ac0a94e75048e5159136a3f1a
Antonio Díaz de León <zucka9517@gmail.com>
2016-12-15 08:02:19 -0700

    Mergeando el p2
    xdxd

---
+++ Becas/Becas/RegistrarAlumnoForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Becas
+{
+    public partial class RegistrarAlumnoForm : Form
+    {
+        public RegistrarAlumnoForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Becas/Becas/principalForm.Designer.cs
+++ Becas/Becas/principalForm.Designer.cs
@@ -32,6 +32,7 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.baseDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.aplicarButton = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -60,6 +61,18 @@
             this.baseDeDatosToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.baseDeDatosToolStripMenuItem.Text = "Base de datos";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(29, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 78);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "                Registrar               Alumno";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // aplicarButton
             // 
             this.aplicarButton.Location = new System.Drawing.Point(388, 153);
@@ -77,6 +90,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(768, 505);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.aplicarButton);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -95,6 +109,7 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem baseDeDatosToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button aplicarButton;
     }
 }
