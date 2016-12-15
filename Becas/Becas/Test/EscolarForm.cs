2be4441fa3dc2b69e398858b65c85043eb0c935c
Emmanuel Espionza <emmanesp13@gmail.com>
2016-12-15 01:15:54 -0700

    Test
    Agrege los test que se haran y ya alv

---
+++ Becas/Becas/Test/EscolarForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Becas.Test
+{
+    public partial class EscolarForm : Form
+    {
+        public EscolarForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
